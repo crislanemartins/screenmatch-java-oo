abbfa8cc67532b2262a8330a5a92fac6d616ac3a
Crislane Martins <crislanegeekdev@gmail.com>
2025-06-11 21:42:12 -0300

    atualizações

--- src/principal.java
+++ src/principal.java
@@ -17,7 +17,7 @@ public class principal {
         meuFilme.avalia(8);
         meuFilme.avalia(5);
         meuFilme.avalia(10);
-        System.out.println("Total de avaliações: " + meuFilme.getTotalDeAvaliacao());
+        System.out.println("Total de avaliacões: " + meuFilme.getTotalDeAvaliacao());
         System.out.println(meuFilme.pegaMedia());
         //meuFilme.somaDasAvaliacoes = 10;
         //meuFilme.totalDeAvaliacao= 1;
